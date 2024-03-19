d3929f5925be30c8846a6e497eb0f7286829352e
rjaramilloh <159493247+rjaramilloh@users.noreply.github.com>
2024-03-18 20:02:26 -0500

    momento 5
     correccion de la clase Zona

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,31 +1,42 @@
 class Zona:
-
-    def __init__ (self, nombre = None, zoo = None, animales = []):
-        self._nombre = nombre
-        self._zoo = Zona (zoo)
-        self._animales = animales
-
-    def agregarAnimales (self, animales):
-        self._animales.append(animales)
-
-    def cantidadAnimales (self):
-        cantidaAnimales = len(self._animales)
-        return cantidaAnimales
     
-    def setNombre(self,nombre):
+    def __init__(self, nombre = None, zoo = None ):
+        
+        self._nombre = nombre
+        self._zoo = zoo
+        self._animales = []
+    
+    def setNombre(self, nombre):
+        
         self._nombre = nombre
     
     def getNombre(self):
+        
         return self._nombre
     
     def setZoo(self, zoo):
+        
         self._zoo = zoo
-    
+        
     def getZoo(self):
+        
         return self._zoo
     
     def setAnimales(self, animales):
+        
         self._animales = animales
-
-    def getAnimales(self):
+        
+    def getAnimales (self):
+        
         return self._animales
+    
+    def agregarAnimales(self, animal):
+        
+        self._animales.append(animal)
+        
+    def cantidadAnimales(self):
+        
+        cantAnimales = len(self._animales)
+        return cantAnimales
+    
+    
